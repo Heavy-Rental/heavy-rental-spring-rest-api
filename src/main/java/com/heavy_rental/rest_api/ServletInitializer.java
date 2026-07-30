1d981ca8c9d7471cf75d42b934d57bff673bb7c8
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2026-07-30 15:54:14 +0000

    initial project commit

---
+++ src/main/java/com/heavy_rental/rest_api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.heavy_rental.rest_api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApiApplication.class);
+	}
+
+}
